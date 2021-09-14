768abc89a24aa3d4f991080537201792f4597a74
Felipe Marinho <felipegigamail@gmail.com>
2021-09-14 04:34:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.send('<h1>Hello world from express</h1>');
 });
 
 server.listen(3000, () => {
